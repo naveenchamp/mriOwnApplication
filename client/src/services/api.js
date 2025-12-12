31fc922d6072aeb177e78527099a1b2727a3a5e6
Naveen reddy tippasani <162026696+naveenchamp@users.noreply.github.com>
2025-12-13 00:56:06 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const baseURL = import.meta.env.VITE_API_URL || "https://mriownapplication-6.onrender.com/";
 
 const api = axios.create({
   baseURL,
